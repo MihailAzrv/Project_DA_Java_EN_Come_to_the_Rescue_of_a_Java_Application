da9567c0a9a5659b849457572f349ea76507f120
Mihail Azarov <mihailazarov92@gmail.com>
2021-09-28 16:59:16 +0200

    Ajout de la classe qui définit l'interface ISymptomTreatement permettant de compter et trier les symptomes à partir du fichier externe

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/CountSymptomFromList.java
@@ -0,0 +1,22 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+import java.util.TreeMap;
+
+public class CountSymptomFromList implements ISymptomTreatement {
+
+	public TreeMap<String, Integer> countSymptoms (List<String> listNotCounted) {
+		
+		TreeMap<String, Integer> count = new TreeMap<String, Integer>();
+		
+		for (String symptom : listNotCounted) {
+			if (!count.containsKey(symptom)) {
+				count.put(symptom, 1);
+			} else if (count.containsKey(symptom)) {
+				count.put(symptom, count.get(symptom) +1);
+			}
+		}
+		return count;
+	}
+	
+}
