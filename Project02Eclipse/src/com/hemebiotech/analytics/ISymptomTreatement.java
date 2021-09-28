ce4c2aafc46aac7685da6151acd6c1f2b22f0a5e
Mihail Azarov <mihailazarov92@gmail.com>
2021-09-28 17:20:47 +0200

    Ajout des imports List et TreeMap

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomTreatement.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomTreatement.java
@@ -1,5 +1,8 @@
 package com.hemebiotech.analytics;
 
+import java.util.List;
+import java.util.TreeMap;
+
 public interface ISymptomTreatement {
 
 	TreeMap<String, Integer> countSymptoms(List<String> listNotCounted);
